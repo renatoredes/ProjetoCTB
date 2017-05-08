22a71d299df44690d9b3741b92189c5f3796b930
Jeferson Junior <jefersonguerrajr@live.com>
2017-05-08 16:02:14 -0300

    Add class Empresa
    Adicionado a classe Empresa ao projeto.
    Falta fazer o Hibernate

---
+++ src/br/com/psc/projetoctb/model/vo/Empresa.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package br.com.psc.projetoctb.model.vo;
+
+/**
+ * @author Jeferson
+ *
+ */
+public class Empresa {
+	
+	private String cnpjEmpresa;
+	private String cepEndereco;
+	private String nomeFantazia;
+	private String razaoSocial;
+	/**
+	 * @return the cnpjEmpresa
+	 */
+	public String getCnpjEmpresa() {
+		return cnpjEmpresa;
+	}
+	/**
+	 * @param cnpjEmpresa the cnpjEmpresa to set
+	 */
+	public void setCnpjEmpresa(String cnpjEmpresa) {
+		this.cnpjEmpresa = cnpjEmpresa;
+	}
+	/**
+	 * @return the cepEndereco
+	 */
+	public String getCepEndereco() {
+		return cepEndereco;
+	}
+	/**
+	 * @param cepEndereco the cepEndereco to set
+	 */
+	public void setCepEndereco(String cepEndereco) {
+		this.cepEndereco = cepEndereco;
+	}
+	/**
+	 * @return the nomeFantazia
+	 */
+	public String getNomeFantazia() {
+		return nomeFantazia;
+	}
+	/**
+	 * @param nomeFantazia the nomeFantazia to set
+	 */
+	public void setNomeFantazia(String nomeFantazia) {
+		this.nomeFantazia = nomeFantazia;
+	}
+	/**
+	 * @return the razaoSocial
+	 */
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	/**
+	 * @param razaoSocial the razaoSocial to set
+	 */
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	
+
+}
