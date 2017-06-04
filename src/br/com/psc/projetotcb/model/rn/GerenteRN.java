54ea8bae30adf0a5643fcf3514d9ead02c501226
CaioPhillipe <cppdesouza@icloud.com>
2017-06-04 10:10:52 -0300

    Atualização RN

--- src/br/com/psc/projetotcb/model/rn/GerenteRN.java
+++ src/br/com/psc/projetotcb/model/rn/GerenteRN.java
@@ -17,6 +17,8 @@ public class GerenteRN {
 	 * 
 	 */
 	
+
+	
 	public void inserirNovo(Gerente g) throws Exception{
 		validaPreenchimento(g);
 		gd.insert(g);		
@@ -24,22 +26,33 @@ public class GerenteRN {
 
 	//Funções internas
 	
-	private void validaPreenchimento(Gerente objeto) throws Exception {
-		if (objeto.getNome() == null)
-			throw new Exception("Nome nao pode ser nulo");
-		if (objeto.getCpf() == null)
-			throw new Exception("CPF nao pode ser nulo");
-		if (objeto.getCelular() == null)
-			throw new Exception("Celular nao pode ser nulo");
-		if (objeto.getEstadoCivil() == null)
-			throw new Exception("Estado Civil nao pode ser nulo");
-		if (objeto.getFuncao() == null)
-			throw new Exception("Funcao nao pode ser nulo");
-		if (objeto.getIdade() == null)
-			throw new Exception("Idade nao pode ser nula");
-		if (objeto.getRg() == null)
-			throw new Exception("RG não pode ser um nulo");
-		if (objeto.getTelefone() == null)
-			throw new Exception("Telefone não pode ser nulo");
-	}
+		public String validarCpf(Gerente objeto) throws Exception{
+			if (objeto.getCpf() == null) {
+				 throw new Exception("Cpf não pode ser Nulo");
+			}else if (objeto.getCpf().isEmpty()) {
+				throw new Exception("Cpf precisa ser preenchido");
+			}
+			return null;
+		}
+	
+		private void validaPreenchimento(Gerente objeto) throws Exception {
+			if (objeto.getNome() == null)
+				throw new Exception("Nome nao pode ser nulo");
+			if (objeto.getCpf() == null)
+				throw new Exception("CPF nao pode ser nulo");
+			if (objeto.getCelular() == null)
+				throw new Exception("Celular nao pode ser nulo");
+			if (objeto.getEstadoCivil() == null)
+				throw new Exception("Estado Civil nao pode ser nulo");
+			if (objeto.getFuncao() == null)
+				throw new Exception("Funcao nao pode ser nulo");
+			if (objeto.getIdade() == null)
+				throw new Exception("Idade nao pode ser nula");
+			if (objeto.getRg() == null)
+				throw new Exception("RG não pode ser um nulo");
+			if (objeto.getTelefone() == null)
+				throw new Exception("Telefone não pode ser nulo");
+		}
+
+		
 }
---
+++ tsw/br/com/Projetoctb/Teste/GerenteTest.java
@@ -0,0 +1,19 @@
+package br.com.Projetoctb.Teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GerenteTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
